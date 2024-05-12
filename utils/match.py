240558f7e5e54aaebc72fc7cd8a41c0c2795eb76
Ruibo Lu <reaperlu@hust.edu.cn>
2024-05-12 21:07:03 +0800

    [utils]: add time metric compare function
    Signed-off-by: Ruibo Lu <reaperlu@hust.edu.cn>

--- utils/match.py
+++ utils/match.py
@@ -33,11 +33,11 @@ def compare_func(cursor: sqlite3.Cursor, func: 'ghidra.program.model.listing.Fun
     cursor.execute(f'SELECT * FROM {FUNC_TABLE_NAME} WHERE hash BETWEEN ? AND ? ORDER BY ABS(hash-?)', (round(hash_ * (1-TOLERANT_)), round(hash_ * (1+TOLERANT_)), hash_))
     results = cursor.fetchall()
     
-    # filter the remain results 
+    # filter the remain results
     matches = []
     max_ = [(1,),0]
     for result_ in results:
-        # compare the 
+        # compare every attributes
         same_ = 1 - diff_percentage(graph, result_[5:9])
         if same_ < ACCEPT_:
             continue
@@ -48,19 +48,52 @@ def compare_func(cursor: sqlite3.Cursor, func: 'ghidra.program.model.listing.Fun
             max_[1] = (same_ + ratio_) / 2
             max_[0] = result_
         matches.append([result_,(same_+ratio_)/2])
-    return (max_, matches) 
+    return (max_, matches)
+
 
 # import for func_key_idx
 def compare_func_db(cursor: sqlite3.Cursor, func_row: tuple) -> list:
+    '''
+    find the most match function and several candidate functions
+    by compare keys we defined and the mnemonic strings
+    '''
+    from .db import FUNC_KEYS, func_key_idx, FUNC_TABLE_NAME
+    hash_ = func_row[func_key_idx('hash')]
+    cursor.execute(f'SELECT * FROM {FUNC_TABLE_NAME} WHERE hash BETWEEN ? AND ? ORDER BY ABS(hash-?)', (round(hash_ * (1-TOLERANT_)), round(hash_ * (1+TOLERANT_)), hash_))
+    results = cursor.fetchall()
+    # filter the remain results
+    matches = []
+    max_ = [(1,),0]
+    for result_ in results:
+        # del the id
+        result_ = result_[1:]
+        # compare every attributes
+        same_ = 1 - diff_percentage(func_row[func_key_idx('block_num'):func_key_idx('jump_num')+1], result_[func_key_idx('block_num'):func_key_idx('jump_num')+1])
+        if same_ < ACCEPT_:
+            continue
+        match = SequenceMatcher(lambda x: x == ',', result_[func_key_idx('mnemonics')], func_row[func_key_idx('mnemonics')], autojunk=False)
+        if match.quick_ratio() < ACCEPT_ or (ratio_ := match.ratio()) < ACCEPT_:
+            continue
+        if max_[1] < (same_ + ratio_):
+            max_[1] = (same_ + ratio_) / 2
+            max_[0] = result_
+        matches.append([result_,(same_+ratio_)/2])
+    return (max_, matches)
+
+import time
+# import for func_key_idx
+def compare_func_db_time(cursor: sqlite3.Cursor, func_row: tuple) -> list:
     '''
     find the most match function and several candidate functions
     by compare keys we defined and the mnemonic strings  
     '''
     from .db import FUNC_KEYS, func_key_idx, FUNC_TABLE_NAME
     hash_ = func_row[func_key_idx('hash')]
+    time_1 = time.time_ns()
     cursor.execute(f'SELECT * FROM {FUNC_TABLE_NAME} WHERE hash BETWEEN ? AND ? ORDER BY ABS(hash-?)', (round(hash_ * (1-TOLERANT_)), round(hash_ * (1+TOLERANT_)), hash_))
     results = cursor.fetchall()
-    
+    time_2 = time.time_ns()
+    first_len = len(results)
     # filter the remain results 
     matches = []
     max_ = [(1,),0]
@@ -78,4 +111,5 @@ def compare_func_db(cursor: sqlite3.Cursor, func_row: tuple) -> list:
             max_[1] = (same_ + ratio_) / 2
             max_[0] = result_
         matches.append([result_,(same_+ratio_)/2])
-    return (max_, matches) 
+    time_3 = time.time_ns()
+    return (max_, matches, time_2 - time_1, time_3 - time_2, first_len, len(matches)) 

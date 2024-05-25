926cdef5d9f5fff9a1590e86c07596ba4b003929
Dingisoul <reaperlu@hust.edu.cn>
2024-05-25 07:52:25 +0000

    [IRQHandler]: move handler creation to valid script
    Signed-off-by: Dingisoul <reaperlu@hust.edu.cn>

--- utils/arm_valid.py
+++ utils/arm_valid.py
@@ -1,4 +1,5 @@
 from pathlib import Path 
+import os
 import json
 '''
 the function used to filter the valid firmware and 
@@ -33,17 +34,17 @@ def firm_valid(firmware: Path) -> Path:
         logging.info(f'Skip: duplicated {firmware} file, skip')
         return None 
     logging.debug(f"Check {firmware.name} arm_valid start")
-    # name valid
-    skip_extentions = ['hex', 'srec', 'ext4', 'wav']
-    for ext_ in skip_extentions:
-        if ext_ in firmware.name:
-            logging.error('Skip: unsupported file extension {ext_} for {firmware}, skip')
-            return None
-    # hard code
-    if firmware.name.startswith('simulator_video'):
-        return None 
-    # hard code
-    if firmware.name.startswith("CoreNatureDictionary.ngram.mini.txt.table.bin"):
+    # skip hard code
+    if 'CoreNatureDictionary.ngram.mini.txt.table.bin.fs3569237fs' in firmware.name:
+        logging.info("Skip CoreNature")
+        return None
+    if 'simulator_video' in firmware.name:
+        logging.info("Skip simulator_video")
+        return None
+    # skip impossible hex
+    # size > 10M 
+    if os.path.getsize(firmware) > 0xa00000 and 'hex' in firmware.name:
+        logging.error(f'Skip: too big {firmware}')
         return None
     # name valid end 
     info_file = firmware.with_name(firmware.name + '_firminfo.json')
@@ -61,7 +62,67 @@ def firm_valid(firmware: Path) -> Path:
     base_address = 0
     # TODO: 0x-1 is hahaha
     if (ba_ := info_.get('base address')) is not None and ba_ != '0x-1': 
-        base_address = int(ba_[2:], base=16)
+        if len(ba_) < 3 or ba_[:2] != '0x':
+            logging.error(f'Skip: invalid base address str {ba_}')
+        else:
+            base_address = int(ba_[2:], base=16)
     arm_base_address[firmware.name] = base_address
     return remove_header(firmware, info_)
+
+# for create handler functions 
+def create_handlers(program: 'ghidra.program.model.listing.Program', flat_api: 'ghidra.program.flatapi') -> int:
+    from ghidra.program.model.symbol import SourceType
+    from ghidra.program.model.symbol import RefType
+    from ghidra.program.model.util   import CodeUnitInsertionException
+    from ghidra.program.model.mem    import MemoryAccessException
+    handler_name = ['MasterStackPointer', 'Reset_Handler', 'NMI_Handler', 'HardFault_Handler', 
+     'MemManage_Handler', 'BusFault_Handler','UsageFault_Handler',
+        'Reserved1_','Reserved2_','Reserved3_','Reserved4_',
+     'SVC_Handler', 'Reserved5_','Reserved6_','PendSV_Handler','SysTick_Handler']
+    i = 0
+    # TODO: use num to count the successfully handler creation not the i
+    num = 0
+    program_len = int(program.getMaxAddress().subtract(program.getMinAddress()))
+    image_base = int(program.getImageBase().getUnsignedOffset())
+    while True:
+        i += 1
+        addr_ = flat_api.toAddr(image_base +4*i)
+        try:
+            handler_address = flat_api.getInt(addr_) - 1
+        except MemoryAccessException:
+            if i >= len(handler_name):
+                break
+            continue
+        if handler_address == -1 or handler_address == 0xfffffffe:
+            try:
+                flat_api.createDWord(addr_)
+            except CodeUnitInsertionException:
+                pass 
+            continue
+        elif handler_address > image_base and (handler_address - image_base) < program_len:
+            if i >= len(handler_name):
+                name_ = 'IRQ' + str(i-16)+ '_Handler'
+            else:
+                name_ = handler_name[i]
+            # create Data and reference 
+            label_ = name_[:name_.find('_')]
+            try:
+                data_ = flat_api.createDWord(addr_)
+                flat_api.createLabel(addr_, label_, True)
+                flat_api.createMemoryReference(data_, flat_api.toAddr(handler_address), RefType.UNCONDITIONAL_CALL)
+            except CodeUnitInsertionException:
+                print(f"\033[31mCreate Handler failed addr:{hex(handler_address)}, name:{name_}\033[0m")
+                continue
+            # create Function 
+            flat_api.disassemble(flat_api.toAddr(handler_address))
+            newfunc = flat_api.createFunction(flat_api.toAddr(handler_address), name_)
+            # rename thunk functions 
+            if newfunc is None:
+                  print(f"\033[31mCreate Function failed addr:{hex(handler_address)}, name:{name_}\033[0m")
+            elif newfunc.getName()[:6] == 'thunk_':
+                newfunc.setName(name_, SourceType.USER_DEFINED)
+        else:
+            return i 
+            # not a correct handler 
+            break
 
--- utils/ghidra_helper.py
+++ utils/ghidra_helper.py
@@ -23,63 +23,6 @@ def get_compiler_spec(lang: "Language", id: str = None) -> "CompilerSpec":
     lang_id = lang.getLanguageID()
     raise ValueError(f"Invalid CompilerSpecID: {id} for Language: {lang_id.toString()}")
 
-# for create handler functions 
-def create_handlers(program: 'ghidra.program.model.listing.Program', flat_api: 'ghidra.program.flatapi') -> int:
-    from ghidra.program.model.symbol import SourceType
-    from ghidra.program.model.symbol import RefType
-    from ghidra.program.model.util   import CodeUnitInsertionException
-    from ghidra.program.model.mem    import MemoryAccessException
-    handler_name = ['MasterStackPointer', 'Reset_Handler', 'NMI_Handler', 'HardFault_Handler', 
-     'MemManage_Handler', 'BusFault_Handler','UsageFault_Handler',
-        'Reserved1_','Reserved2_','Reserved3_','Reserved4_',
-     'SVC_Handler', 'Reserved5_','Reserved6_','PendSV_Handler','SysTick_Handler']
-    i = 0
-    # TODO: use num to count the successfully handler creation not the i
-    num = 0
-    program_len = int(program.getMaxAddress().subtract(program.getMinAddress()))
-    image_base = int(program.getImageBase().getUnsignedOffset())
-    while True:
-        i += 1
-        addr_ = flat_api.toAddr(image_base +4*i)
-        try:
-            handler_address = flat_api.getInt(addr_) - 1
-        except MemoryAccessException:
-            if i >= len(handler_name):
-                break
-            continue
-        if handler_address == -1 or handler_address == 0xfffffffe:
-            try:
-                flat_api.createDWord(addr_)
-            except CodeUnitInsertionException:
-                pass 
-            continue
-        elif handler_address > image_base and (handler_address - image_base) < program_len:
-            if i >= len(handler_name):
-                name_ = 'IRQ' + str(i-16)+ '_Handler'
-            else:
-                name_ = handler_name[i]
-            # create Data and reference 
-            label_ = name_[:name_.find('_')]
-            try:
-                data_ = flat_api.createDWord(addr_)
-                flat_api.createLabel(addr_, label_, True)
-                flat_api.createMemoryReference(data_, flat_api.toAddr(handler_address), RefType.UNCONDITIONAL_CALL)
-            except CodeUnitInsertionException:
-                print(f"\033[31mCreate Handler failed addr:{hex(handler_address)}, name:{name_}\033[0m")
-                continue
-            # create Function 
-            flat_api.disassemble(flat_api.toAddr(handler_address))
-            newfunc = flat_api.createFunction(flat_api.toAddr(handler_address), name_)
-            # rename thunk functions 
-            if newfunc is None:
-                  print(f"\033[31mCreate Function failed addr:{hex(handler_address)}, name:{name_}\033[0m")
-            elif newfunc.getName()[:6] == 'thunk_':
-                newfunc.setName(name_, SourceType.USER_DEFINED)
-        else:
-            return i 
-            # not a correct handler 
-            break
-
 def openProject(project_name: str, project_location: Path) -> 'ghidra.base.project.GhidraProject': 
     from java.io import IOException
     from ghidra.base.project import GhidraProject

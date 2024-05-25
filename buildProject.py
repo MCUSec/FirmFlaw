cf86e501e0ceece1973c0d5faae873f3e1f769e5
Dingisoul <reaperlu@hust.edu.cn>
2024-05-25 07:46:10 +0000

    [build_project]: fix elf handler and save when ctrl+c
    1. fix the elf handler when build program
    2. add auto save when ctrl+c keyboard interrupt
    
    Signed-off-by: Dingisoul <reaperlu@hust.edu.cn>

--- buildProject.py
+++ buildProject.py
@@ -10,12 +10,26 @@ from utils.ghidra_helper import *
 from utils.launcher import HeadlessLoggingPyhidraLauncher
 
 log_time = time.strftime("%Y-%m-%d_%H:%M:%S")
+func_num = None
+project = None
+start_time = None
 
 def timeout_handler(signum, frame):
     raise TimeoutError("Timed out!")
 
-def main(args):
+def firmware_size(dir: Path) -> dict:
+    firmware_size_ = {}
+    for root,dirs,files in os.walk(dir):
+        files = [f for f in files if not f.endswith('json')]
+        dir_ = Path(root)
+        for f in files:
+            firmware_size_[f] = os.path.getsize(dir_/f)
+    return firmware_size_
 
+def main(args):
+    global func_num
+    global project
+    global start_time
     # pyhidra launcher 
     launcher = HeadlessLoggingPyhidraLauncher(verbose=True, log_path=f'./logs/Pyhidra_{args.project_name}_{log_time}.log')
     launcher.start()
@@ -43,11 +57,25 @@ def main(args):
     lang = get_language(lang_str())
 
     exist_bins_ = set()
-    for file_ in project.getRootFolder().getFiles():
-        exist_bins_.add(file_.getName())
-
-    num = 0
     func_num = []
+    firm_size = firmware_size(args.file_path)
+    num = 0
+    for file_ in project.getRootFolder().getFiles():
+        noheader_ = file_.getName()
+        name_ = noheader_[:noheader_.find('noheader')]
+        program_ = project.openProgram('/', noheader_, True)
+        funcs_ = program_.getFunctionManager().getFunctionCount()
+        if funcs_ == 0:
+            flat_api = FlatProgramAPI(program_)
+            flat_api.analyzeAll(program_)
+        funcs_ = program_.getFunctionManager().getFunctionCount()
+        if (size_ := firm_size.get(name_)) is None:
+            logging.error(f'Cannot get size of {name_}')
+        func_num.append([name_, 0, funcs_, size_, 0])
+        project.close(program_)
+        exist_bins_.add(noheader_)
+        num += 1
+
     start_time = time.time()
     analysis_time = time.time()
     signal.signal(signal.SIGALRM, timeout_handler)
@@ -57,13 +85,17 @@ def main(args):
         dir_ = Path(root)
         for file_ in firmwares: 
             firm_ = dir_ / file_
-            noheader_ = firm_valid(dir_ / file_)
+            if file_.endswith('elf'):
+                noheader_ = firm_
+            else:
+                noheader_ = firm_valid(firm_)
             if noheader_ is None:
                 continue
             logging.info(f"Iter file {firm_} at {num}")
             monitor.setMessage(f"\033[31mIter file {firm_} at {num}\033[0m")
             # firmware has been analyzed 
-            if noheader_ in exist_bins_:
+            if noheader_.name in exist_bins_:
+                print(f"skip")
                 logging.info(f"Skip file {firm_} at {num} because ghidra project exist")
                 continue 
             # timeout and try 
@@ -98,15 +130,11 @@ def main(args):
                 signal.alarm(0)
             project.saveAs(program, "/", program.getName(), True)
             project.close(program)
-            break 
     # write csv 
     with open(f'./res/func_num_{project_name}.csv', 'w') as file:
         file.write('Program, Handlers, Functions, Size, AnalysisTime\n')
         for i in func_num:
-            line = ""
-            for j in i:
-                line += str(j) + ", "
-            line = line[:-2] + '\n'
+            line = ', '.join(str(j) for j in i) + '\n'
             file.write(line)
     # end
     logging.info("Finish: total annalysis time {time.time() - start_time}")
@@ -134,5 +162,15 @@ if __name__ == "__main__":
                 exec(file.read())
             main(args)
     except KeyboardInterrupt:
+        if start_time is not None:
+            logging.info("Finish: total annalysis time {time.time() - start_time}")
         logging.error("Exit with keyboard")
         project.close() 
+        if func_num is not None:
+            # write csv
+            with open(f'./res/func_num_{project_name}.csv', 'w') as file:
+                file.write('Program, Handlers, Functions, Size, AnalysisTime\n')
+                for i in func_num:
+                    line = ', '.join(j for j in i) + '\n'
+                    file.write(line)
+            # end
